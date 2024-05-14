c6f495e7d332b389f2f4b4ae52c64135b742f30f
liahrothstein <liahrothstein@gmail.com>
2024-05-14 20:02:38 +0300

    feat: add main page styles

--- src/pages/main-page/ui/MainPage.tsx
+++ src/pages/main-page/ui/MainPage.tsx
@@ -1,10 +1,14 @@
-import { Layout } from 'antd';
+import { Layout, Typography } from 'antd';
+
+import './MainPage.scss';
 
 export function MainPage() {
     const { Content } = Layout;
+    const { Title } = Typography;
 
     return (
         <Content>
+            <Title level={2}>⬅ Пожалуйста, выберите рецепт в списке</Title>
         </Content>
     )
 }
